666b7bd4fb43ec71fb6afc0923543aabf9559d83
Taworn Ta <taworn.ta7@gmail.com>
2022-12-30 03:11:01 +0700

    - add test_text_template_speed

---
+++ test_text_template_speed/src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const test_handlebars = require('./test_handlebars');
+const test_pug = require('./test_pug');
+const test_ejs = require('./test_ejs');
+
+test_handlebars();
+test_pug();
+test_ejs();
